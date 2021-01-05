d9676748650e97e9eacdaebaecd1b19901c852ad
Ibrahim Saberi <georgeipsum@gmail.com>
2021-01-05 11:36:34 -0500

    whatever

--- a.js
+++ a.js
@@ -1,4 +1,5 @@
 const EORZEA_RATIO = 3600 / 175;
+const DAY_IN_MS = 1000 * 60 * 60 * 24;
 const GLOBAL = {
   utcTime: null,
   eorzeaTime: null
@@ -10,6 +11,7 @@ const update = () => {
   GLOBAL.eorzeaTime = new Date();
   GLOBAL.eorzeaTime.setTime(E_TIMESTAMP);
   updateDocument();
+  updateTimeUntil();
 };
 
 const updateDocument = () => {
@@ -26,11 +28,35 @@ const updateDocument = () => {
   eorzeaTimeEl.innerHTML = `${ hours }:${ minutes } ${ aMPMString }`;
 };
 
+const getTimeElapsedSinceDayStart = (inputDate) => inputDate.getUTCMilliseconds() + (inputDate.getUTCSeconds() * 1000) + (inputDate.getUTCMinutes() * 60 * 1000) + (inputDate.getUTCHours() * 60 * 60 * 1000);
+
+const updateTimeUntil = () => {
+  const inputDate = document.getElementById("offset-timer").valueAsDate;
+  const inputMs = getTimeElapsedSinceDayStart(inputDate);
+  const ETMs = getTimeElapsedSinceDayStart(GLOBAL.eorzeaTime);
+  let diff;
+  if (ETMs > inputMs) {
+    const timeLeftInDay = DAY_IN_MS - ETMs;
+    diff = timeLeftInDay + inputMs;
+  } else {
+    diff = inputMs - ETMs;
+  }
+  diff /= 1000;
+  // time left in seconds
+  const EARTH_UNITS = Math.floor(diff / EORZEA_RATIO);
+  const timeUntilEl = document.getElementById("time-until");
+  timeUntilEl.innerHTML = `: ${Math.floor(EARTH_UNITS / 60)} Minutes`;
+};
+
 function padLeft(val) {
   var str = "" + val;
   var pad = "00";
   return pad.substring(0, pad.length - str.length) + str;
 }
 
-window.setInterval(update, Math.floor((1000 * 60) / EORZEA_RATIO));
-update();
+document.addEventListener("DOMContentLoaded", () => {
+  window.setInterval(update, Math.floor((1000 * 60) / EORZEA_RATIO));
+  update();
+  const inputEl = document.getElementById("offset-timer");
+  inputEl.addEventListener('input', updateTimeUntil);
+});
